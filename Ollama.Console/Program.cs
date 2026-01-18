4f618f65ccf18bc3b8ff76613a2d78e847c045e3
Leonardo Lima <leonardolimaf54@gmail.com>
2026-01-18 15:16:20 -0300

    console integrado ao ollama utilizando o modelo gpt-oss

--- Ollama.Console/Program.cs
+++ Ollama.Console/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
 
-Console.WriteLine("Hello, World!");
+IChatClient clientChat = new OllamaApiClient(new Uri("http://localhost:11434"), "gpt-oss:20b");
+
+var historicoChat = new List<ChatMessage>();
+
+Console.WriteLine("Digite 'sair' para encerrar");
+Console.WriteLine();
+
+while (true)
+{
+    Console.Write(">> ");
+    var entrada = Console.ReadLine();
+    
+    if (entrada?.ToLower() == "sair")
+        break;
+    
+    if (string.IsNullOrWhiteSpace(entrada))
+        continue;
+    
+    historicoChat.Add(new ChatMessage(ChatRole.User, entrada));
+
+    Console.WriteLine("");
+    var resposta = string.Empty;
+
+    await foreach (var atualizacao in clientChat.GetStreamingResponseAsync(historicoChat))
+    {
+        Console.Write(atualizacao.Text);
+        resposta += atualizacao.Text;
+    }
+    
+    historicoChat.Add(new ChatMessage(ChatRole.Assistant, resposta));
+    Console.WriteLine("\n");
+}
